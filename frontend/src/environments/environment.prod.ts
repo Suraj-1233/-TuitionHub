95b050d5b5157d6c83a41c801dd5a3b327efe838
surajkannujiya <surajkannujiya517@gmail.com>
2026-04-25 16:34:20 +0530

    Fix: Add environment files for Docker build

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: '/api', // Relative path for production (assuming same domain)
+  razorpayKey: 'rzp_live_YOUR_PROD_KEY',
+  googleClientId: '75996591633-bsem1o0tef4smplnlrabh20rf4ar3ej0.apps.googleusercontent.com'
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api',
+  razorpayKey: 'rzp_test_YOUR_DEV_KEY',
+  googleClientId: '75996591633-bsem1o0tef4smplnlrabh20rf4ar3ej0.apps.googleusercontent.com'
+};
